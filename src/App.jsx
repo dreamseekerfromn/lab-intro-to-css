ff0599dbc97a76efbc024b655e0b88ed873c3a1a
Sung Yi <sy@Sungs-Air.fios-router.home>
2023-07-26 21:29:05 -0400

    changing to react

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <>
+            <h1>Sung Yi</h1>
+            <div>
+                <p>
+                    This is a bunch of information about myself. I'm from here and there and
+                    discovered my love of programming when this happened. When I'm not
+                    working I'm busy doing this and that.
+                </p>
+            </div>
+
+            <img
+            src="https://images.pexels.com/photos/1170986/pexels-photo-1170986.jpeg"
+            alt=""
+            />
+
+            <div>
+                <h3>Here are some of my Skills!</h3>
+                <ul>
+                    <h3>Languages</h3>
+                    <li>JavaScript</li>
+                    <li>SQL</li>
+                    <li>HTML5</li>
+                    <li>CSS3</li>
+                </ul>
+
+                <ol>
+                    <h3>Libraries</h3>
+                    <li>React</li>
+                    <li>PostgreSQL</li>
+                    <li>Node</li>
+                    <li>Bootstrap</li>
+                </ol>
+            </div>
+
+            <div>
+            <h3>Hardest Bug So Far</h3>
+                <p>
+                    My hardest bug I ever came across was this infinite loop I couldn't
+                    escape.
+                </p>
+                <p>I came up with a totally sick solution though by doing ...</p>
+            </div>
+
+            <h2>Contact Me</h2>
+            <div>
+                Email me at:
+                <a href="mailto:hello@pursuit.org" target="_top">hello@pursuit.org</a>
+            </div>
+
+            <ul>
+                <li><a href="github.com">github link</a></li>
+                <li><a href="linkedin.com">LinkedIn link</a></li>
+                <li><a href="angellist.com">Angel list link</a></li>
+            </ul>
+        </>
+    </React.StrictMode>
+)
